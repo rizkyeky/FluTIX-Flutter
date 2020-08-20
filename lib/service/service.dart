7524311a7f81ef1d3f152a7ae15761d6774eeb2e
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-20 23:29:53 +0700

    update service part

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,4 +1,10 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:flutix_training/extension/extension.dart';
+
+import 'package:flutix_training/model/model.dart' as model;
 
 part 'service_auth.dart';
+part 'service_user.dart';
