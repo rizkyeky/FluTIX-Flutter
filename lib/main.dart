9fef60696d7f0c47c30ba22e052c26b0f500f72e
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-21 11:08:01 +0700

    add service core init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:flutix_training/service/service.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await CoreService.init();
   runApp(MyApp());
 }
 
@@ -27,7 +31,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: Text("$title"),
       ),
       body: Center(
         child: Column(
@@ -37,17 +41,46 @@ class MyHomePage extends StatelessWidget {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$title',
+              '1',
               style: Theme.of(context).textTheme.headline4,
             ),
+            RaisedButton(
+              child: Text("Sign Up"),
+              onPressed: () async {
+                AuthResult result = await AuthService.signUp(
+                  'Rizky Eky',
+                  'rizeky@gmail.com',
+                  '123456',
+                  ['Romace', 'Action', 'Adventure'],
+                  ['Western', 'Indonesia', 'Korea'],
+                );
+
+                if (result.user == null) {
+                  print(result.message);
+                } else {
+                  print(result.user.toString());
+                }
+              }
+            ),
+            RaisedButton(
+              child: Text("Sign In"),
+              onPressed: () async {
+                AuthResult result = await AuthService.signIn(
+                  'rizeky@gmail.com',
+                  '123456',
+                );
+
+                if (result.user == null) {
+                  print(result.message);
+                } else {
+                  print(result.user.toString());
+                }
+              }
+            )
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: (),
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ),
     );
   }
 }
+
