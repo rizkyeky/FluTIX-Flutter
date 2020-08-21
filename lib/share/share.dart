863fb7d90f4cee3f65c1dc202136b5e5f8320112
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-21 23:03:19 +0700

    add share theme

--- lib/share/share.dart
+++ lib/share/share.dart
@@ -1,2 +1,8 @@
+import 'package:flutix_training/main.dart';
+import 'package:flutter/painting.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 part 'share_variable.dart';
 part 'share_key.dart';
+
+part 'share_theme.dart';
---
+++ lib/share/share_theme.dart
@@ -0,0 +1,31 @@
+part of 'share.dart';
+
+const Color mainColor = Color(0xFF2C3493);
+const Color accentColor = Color(0xFFE34B9E);
+const Color blackColor = Color(0xFF505152);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color canvasColor = Color(0xFFF4F4F4);
+
+TextStyle blueTitle = GoogleFonts.ptSans()
+  .copyWith(color: mainColor, fontSize: 24, fontWeight: FontWeight.bold);
+
+TextStyle whiteTitle = GoogleFonts.ptSans()
+  .copyWith(color: whiteColor, fontSize: 24, fontWeight: FontWeight.bold);
+   
+TextStyle blackSubtitle = GoogleFonts.ptSans()
+  .copyWith(color: blackColor, fontSize: 18, fontWeight: FontWeight.normal);
+
+TextStyle blueSubtitle = GoogleFonts.ptSans()
+  .copyWith(color: mainColor, fontSize: 18, fontWeight: FontWeight.bold);
+
+TextStyle whiteSubtitle = GoogleFonts.ptSans()
+  .copyWith(color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold);
+
+TextStyle blackContentRegular = GoogleFonts.ptSans()
+  .copyWith(color: blackColor, fontSize: 12, fontWeight: FontWeight.normal);
+
+TextStyle blackContentBold = GoogleFonts.ptSans()
+  .copyWith(color: blackColor, fontSize: 12, fontWeight: FontWeight.bold);
+
+TextStyle whiteContent = GoogleFonts.ptSans()
+  .copyWith(color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold);
