426f619c67cbccc7008fc2657022e784db559cd0
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-22 19:17:38 +0700

    update page

--- lib/view/page/page.dart
+++ lib/view/page/page.dart
@@ -1,5 +1,8 @@
+import 'package:flutix_training/bloc/bloc.dart';
 import 'package:flutix_training/view/component/component.dart';
 import 'package:flutter/material.dart';
 import 'package:flutix_training/share/share.dart';
 
-part 'signin.dart';
+part 'page_signin.dart';
+part 'page_signup.dart';
+part 'page_favorite.dart';
