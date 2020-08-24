7afa3028897c83e3d7804db43c76cb3dfbb868f9
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-24 15:43:58 +0700

    update theme

--- lib/share/share_theme.dart
+++ lib/share/share_theme.dart
@@ -1,41 +1,36 @@
 part of 'share.dart';
 
-const EdgeInsets paddingPage = EdgeInsets.only(left: 48,top: 48, right: 48, bottom: 48);
+const EdgeInsets paddingPage =
+    EdgeInsets.only(left: 48, top: 48, right: 48, bottom: 48);
 
 const Color mainColor = Color(0xFF2C3493);
 const Color accentColor = Color(0xFFE34B9E);
 const Color blackColor = Color(0xFF505152);
 const Color whiteColor = Color(0xFFFFFFFF);
 const Color canvasColor = Color(0xFFF4F4F4);
+const Color starColor = Color(0xFFF2C94C);
 
 TextStyle blueTitle = GoogleFonts.ptSans(
-  color: mainColor, fontSize: 24, fontWeight: FontWeight.bold
-);
+    color: mainColor, fontSize: 24, fontWeight: FontWeight.bold);
 
 TextStyle whiteTitle = GoogleFonts.ptSans(
-  color: whiteColor, fontSize: 24, fontWeight: FontWeight.bold
-);
-   
+    color: whiteColor, fontSize: 24, fontWeight: FontWeight.bold);
+
 TextStyle blackSubtitle = GoogleFonts.ptSans(
-  color: blackColor, fontSize: 18, fontWeight: FontWeight.normal
-);
+    color: blackColor, fontSize: 18, fontWeight: FontWeight.normal);
 
 TextStyle blueSubtitle = GoogleFonts.ptSans(
-  color: mainColor, fontSize: 18, fontWeight: FontWeight.bold
-);
+    color: mainColor, fontSize: 18, fontWeight: FontWeight.bold);
 
 TextStyle whiteSubtitle = GoogleFonts.ptSans(
-  color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold
-);
+    color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold);
 
 TextStyle blackContentRegular = GoogleFonts.ptSans(
-  color: blackColor, fontSize: 12, fontWeight: FontWeight.normal
+  color: blackColor, fontSize: 16, fontWeight: FontWeight.normal
 );
 
 TextStyle blackContentBold = GoogleFonts.ptSans(
-  color: blackColor, fontSize: 12, fontWeight: FontWeight.bold
-);
+    color: blackColor, fontSize: 16, fontWeight: FontWeight.bold);
 
 TextStyle whiteContent = GoogleFonts.ptSans(
-  color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold
-);
+    color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold);
