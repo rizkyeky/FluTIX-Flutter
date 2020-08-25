a6cda21216dedac73c77b5027ac1e5fcb79bc007
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-25 22:50:17 +0700

    finish movie card for playing now

---
+++ lib/view/component/movie_card.dart
@@ -0,0 +1,62 @@
+part of 'component.dart';
+
+class MovieCard extends StatelessWidget {
+
+  final Movie _movie;
+
+  const MovieCard(this._movie, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final int starCount = (_movie.rating/2).round();
+
+    return XCard(
+      padding: const EdgeInsets.symmetric(horizontal: 6),
+      backgroundImage: NetworkImage('${imageBaseURL}w500${_movie.backdropPath}'),
+      child: Container(
+        width: 360,
+        padding: const EdgeInsets.all(12),
+        alignment: Alignment.bottomCenter,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              Colors.black.withOpacity(0.6),
+              Colors.black.withOpacity(0),
+            ]
+          )
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(_movie.title,
+                  maxLines: 2,
+                  style: whiteTitle,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  children: [
+                    for (int i = 0; i < starCount; i++) const Icon(Icons.star, 
+                      size: 18,
+                      color: starColor
+                    ),
+                    const SizedBox(width: 6,),
+                    Text('${_movie.rating}/10', style: whiteContent)
+                  ],
+                )
+              ],
+            ),
+            IconButton(icon: Icon(Icons.favorite, color: whiteColor,), onPressed: () {},)
+          ],
+        )
+      ),
+    );
+  }
+}
