ecc78565f70b4661a1b7d87c1146c5f5a6aee2d6
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-25 14:31:22 +0700

    add http package

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart' as fire_auth;
 import 'package:cloud_firestore/cloud_firestore.dart'  as fire_store;
 import 'package:firebase_core/firebase_core.dart' as fire_core;
+import 'package:http/http.dart' as http;
 
 // import 'package:flutix_training/extension/extension.dart';
 
 import 'package:flutix_training/model/model.dart';
+import 'package:flutix_training/share/share.dart';
 
 part 'service_auth.dart';
 part 'service_user.dart';
 part 'service_core.dart';
+part 'service_movie.dart';
