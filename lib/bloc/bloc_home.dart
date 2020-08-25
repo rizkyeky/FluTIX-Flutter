ddc8d3503e256cf9d9c5c84b302bccf21a5018df
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-25 19:49:44 +0700

    start make home bloc

--- lib/bloc/bloc_home.dart
+++ lib/bloc/bloc_home.dart
@@ -1,7 +1,21 @@
 part of 'bloc.dart';
 
 class HomeBloc {
+
+  final MovieService _movieService = MovieService();
+
   final BehaviorSubject<bool> _isLoadingController = BehaviorSubject();
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
+  List<Movie> _movies = [];
+
+  Future<void> getMoviesFromService() async {
+    if (_movies.isEmpty) {
+      _movies = await _movieService.getMovies();
+    }
+  }
+
+  Future<void> getMovieFromIndex() async {
+    
+  }
 }
