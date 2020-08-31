cab3475203c8497d767b9e90c1d939e318254ac7
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-31 08:13:05 +0700

    update movie model

--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -51,36 +51,51 @@ class Movie {
 
   factory Movie.fromJson(Map<String, dynamic> json) {
 
-    // print(json['runtime']);
-
     return Movie(
-    json['id'] as int,
-    json['title'] as String,
-    (json['popularity'] as num).toDouble(),
-    json['release_date'] as String,
-    json['poster_path'] as String,
-    json['backdrop_path'] as String,
-    json['overview'] as String,
-    (json['vote_count'] as num).toInt(),
-    (json['vote_average'] as num).toDouble(),
-    json['original_language'] as String,
+      json['id'] as int,
+      json['title'] as String,
+      (json['popularity'] as num).toDouble(),
+      json['release_date'] as String,
+      json['poster_path'] as String,
+      json['backdrop_path'] as String,
+      json['overview'] as String,
+      (json['vote_count'] as num).toInt(),
+      (json['vote_average'] as num).toDouble(),
+      json['original_language'] as String,
 
-    ((json['genre_ids'] as List) ?? (json['genres'] as List))
-      .map((e) {
-        if (e is int) {
-          return Genre.fromID(e);
-        }
-        else if (e is Map) {
-          return Genre.fromID(e['id'] as int);
-        }
-        else {
-          return Genre.fromID(0);
-        } 
-      }).toList(),
+      ((json['genre_ids'] as List) ?? (json['genres'] as List))
+        .map((e) {
+          if (e is int) {
+            return Genre.fromID(e);
+          }
+          else if (e is Map) {
+            return Genre.fromID(e['id'] as int);
+          }
+          else {
+            return Genre.fromID(0);
+          } 
+        }).toList(),
 
-    (json['status'] as String) ?? '',
-    (json['runtime'] as int) ?? 0,
-  );
+      (json['status'] as String) ?? '',
+      (json['runtime'] as int) ?? 0,
+    );
+  }
+
+  void duplicate(Movie movie) {
+    id = movie.id ?? id;
+    title = movie.title ?? title;
+    popularity = movie.popularity ?? popularity;
+    releaseDate = movie.releaseDate ?? releaseDate;
+    posterPath = movie. posterPath ?? posterPath;
+    backdropPath = movie.backdropPath ?? backdropPath;
+    overview = movie.overview ?? overview;
+    voteCount = movie.voteCount ?? voteCount;
+    voteAverage = movie.voteAverage ?? voteAverage;
+    country = movie.country ?? country;
+    genres = movie.genres ?? genres;
+
+    status = movie.status ?? status;
+    runtime = movie.runtime ?? runtime;
   }
 
   @override
