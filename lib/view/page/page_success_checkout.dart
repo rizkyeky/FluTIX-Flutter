15051694729659e861188bb33f261cddb77b3f7c
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-01 21:36:56 +0700

    add and update checkout page

--- lib/view/page/page_success_checkout.dart
+++ lib/view/page/page_success_checkout.dart
@@ -7,12 +7,31 @@ class SuccessCheckoutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          SizedBox(
-            height: 300,
-            child: Icon(Icons.check_circle, color: mainColor, size: 36,),
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            height: 200,
+            child: const Icon(Icons.check_circle, color: mainColor, size: 54,),
           ),
-        
+          Text('Checkout Success', style: blueTitle),
+          Text("Enjoy your movie and\ndon't forget to bring your popcorns", 
+            style: blackSubtitle, textAlign: TextAlign.center
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: 100,
+            child: FlatButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context, 
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false
+                );
+              }, 
+              child: Text('Back to home', style: blueSubtitle),
+            ),
+          ),
         ],
       ),
     );
