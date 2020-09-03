07e8d4f2a30b4980fe42fc7457a31a2ba6dbc545
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-03 12:57:01 +0700

    add signin and signup bloc

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -1,12 +1,11 @@
-import 'dart:async';
 
 import 'package:flutix_training/model/model.dart';
 import 'package:rxdart/rxdart.dart';
 
 import 'package:flutix_training/service/service.dart';
 
-part 'bloc_sigin.dart';
-part 'bloc_sigup.dart';
+part 'bloc_signin.dart';
+part 'bloc_signup.dart';
 part 'bloc_home.dart';
 part 'bloc_maintabbar.dart';
 part 'bloc_detailmovie.dart';
