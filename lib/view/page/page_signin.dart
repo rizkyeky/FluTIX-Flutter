a9d9109a5ceeb94f0f824eb3363e4de645db9a8b
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-05 19:32:59 +0700

    add text editing controller in page

--- lib/view/page/page_signin.dart
+++ lib/view/page/page_signin.dart
@@ -1,62 +1,80 @@
 part of 'page.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends Page<SignInBloc> {
   SignInPage({Key key}) : super(key: key);
 
-  final SignInBloc bloc = SignInBloc();
+  final TextEditingController _emailText = TextEditingController();
+  final TextEditingController _passwordText = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: TopLinearProgressIndicator(
-      //   backgroundColor: mainColor,
-      //   valueColor:accentColor,
-      //   stream: bloc.isLoadingStream,
-      // ),
-      body: SingleChildScrollView(
-        padding: paddingPage,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Image.asset('assets/logo/flutix.png',
-              width: 100,
-            ),
-            const SizedBox(height: 36,),
-            Text('Exclusive movies are\nwaiting for you',
-              style: blueTitle,
-            ),
-            const SizedBox(height: 36,),
-            const XTextField(
-              text: 'Email',
-            ),
-            const SizedBox(height: 36,),
-            const XTextField(
-              text: 'Password',
-            ),
-            const SizedBox(height: 36,),
-            BlueRectButton(
-              text: "Let's go",
-              onTap: () {
-                // bloc.setLoading()
-              },
-            ),
-            const SizedBox(height: 36,),
-            Row(
-              children: [
-                Text("Don't have account ? ",
-                  style: blackSubtitle,
-                ),
-                GestureDetector(
-                  onTap: () => Navigator.pushReplacementNamed(context, '/signup'),
-                  child: Text('Join now',
-                    style: blueSubtitle,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        // appBar: TopLinearProgressIndicator(
+        //   backgroundColor: mainColor,
+        //   valueColor:accentColor,
+        //   stream: bloc.isLoadingStream,
+        // ),
+        body: SingleChildScrollView(
+          padding: paddingPage,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.asset('assets/logo/flutix.png',
+                width: 100,
+              ),
+              const SizedBox(height: 36,),
+              Text('Exclusive movies are\nwaiting for you',
+                style: blueTitle,
+              ),
+              const SizedBox(height: 36,),
+              XTextField(
+                controller: _emailText,
+                text: 'Email',
+              ),
+              const SizedBox(height: 36,),
+              XTextField(
+                controller: _passwordText,
+                text: 'Password',
+              ),
+              const SizedBox(height: 36,),
+              BlueRectButton(
+                text: "Let's go",
+                onTap: () async{
+                  await bloc.signIn(_emailText.text, _passwordText.text);
+                  print('tap');
+                },
+              ),
+              const SizedBox(height: 36,),
+              Row(
+                children: [
+                  Text("Don't have account ? ",
+                    style: blackSubtitle,
                   ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ) 
+                  GestureDetector(
+                    onTap: () => Navigator.pushReplacementNamed(context, '/signup'),
+                    child: Text('Join now',
+                      style: blueSubtitle,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ) 
+      ),
     );
   }
+
 }
--- lib/view/page/page_signup.dart
+++ lib/view/page/page_signup.dart
@@ -3,58 +3,68 @@ part of 'page.dart';
 class SignUpPage extends Page<SignUpBloc> {
   SignUpPage({Key key}) : super(key: key);
 
+  final TextEditingController _nameText = TextEditingController();
+  final TextEditingController _emailText = TextEditingController();
+  final TextEditingController _passwordText = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: TopLinearProgressIndicator(
-        backgroundColor: mainColor,
-        valueColor:accentColor,
-        stream: bloc.loadingStream,
-      ),
-      body: SingleChildScrollView(
-        padding: paddingPage,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Let's start your\nexperience now",
-              style: blueTitle,
-            ),
-            const SizedBox(height: 36,),
-            const XTextField(
-              text: 'Name',
-            ),
-            const SizedBox(height: 36,),
-            const XTextField(
-              text: 'Email',
-            ),
-            const SizedBox(height: 36,),
-            const XTextField(
-              text: 'Password',
-            ),
-            const SizedBox(height: 36,),
-            BlueRectButton(
-              text: "Let's start",
-              onTap: () {
-                // bloc.setLoading()
-              },
-            ),
-            const SizedBox(height: 36,),
-            Row(
-              children: [
-                Text('Already have account ? ',
-                  style: blackSubtitle,
-                ),
-                GestureDetector(
-                  onTap: () => Navigator.pushReplacementNamed(context, '/signin'),
-                  child: Text('Join now',
-                    style: blueSubtitle,
-                  ),
-                ),
-              ],
-            )
-          ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: TopLinearProgressIndicator(
+          backgroundColor: mainColor,
+          valueColor:accentColor,
+          stream: bloc.loadingStream,
         ),
-      ) 
+        body: SingleChildScrollView(
+          padding: paddingPage,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Let's start your\nexperience now",
+                style: blueTitle,
+              ),
+              const SizedBox(height: 36,),
+              XTextField(
+                controller: _nameText,
+                text: 'Name',
+              ),
+              const SizedBox(height: 36,),
+              XTextField(
+                controller: _emailText,
+                text: 'Email',
+              ),
+              const SizedBox(height: 36,),
+              XTextField(
+                controller: _passwordText,
+                text: 'Password',
+              ),
+              const SizedBox(height: 36,),
+              BlueRectButton(
+                text: "Let's start",
+                onTap: () {
+                  bloc.signUp(_nameText.text, _emailText.text, _passwordText.text);
+                },
+              ),
+              const SizedBox(height: 36,),
+              Row(
+                children: [
+                  Text('Already have account ? ',
+                    style: blackSubtitle,
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.pushReplacementNamed(context, '/signin'),
+                    child: Text('Join now',
+                      style: blueSubtitle,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ) 
+      ),
     );
   }
 
