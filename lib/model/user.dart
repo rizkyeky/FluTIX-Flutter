e3006d5fdc7e6a13db707db3825e9a16e4406fa8
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-07 21:27:02 +0700

    fix and update user model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,21 +1,46 @@
 part of 'model.dart';
 
 class User{
-  final String id;
-  final String name;
-  final String email;
+  String id;
+  String name;
+  String email;
   // final String password;
-  final String photoURL;
-  final List<String> favoriteGenre; 
-  final List<String> favoriteCountry;
+  String photoURL;
+  List<String> favoriteGenre; 
+  List<String> favoriteCountry;
 
-  const User(this.id, this.name, this.email,
+  User(this.id, this.name, this.email,
   this.photoURL, this.favoriteGenre, this.favoriteCountry);
 
-  factory User.initial() => const User(
+  factory User.initial() => User(
     '','','','',[],[] 
   );
 
   @override
   String toString() => '$id - $name, $email';
+
+  void copyWith({
+    String id,
+    String name,
+    String email,
+    String photoURL, 
+    List<String> favoriteGenre,
+    List<String> favoriteCountry
+  }) {
+    this.id = id ?? this.id;
+    this.name = name ?? this.name;
+    this.email = email ?? this.email;
+    this.photoURL = photoURL ?? this.photoURL;
+    this.favoriteGenre = favoriteGenre ?? this.favoriteGenre;
+    this.favoriteCountry = favoriteCountry ?? this.favoriteCountry;
+  }
+
+  void duplicate(User user) {
+    id = user.id;
+    name = user.name;
+    email = user.email;
+    photoURL = user.photoURL;
+    favoriteGenre = user.favoriteGenre;
+    favoriteCountry = user.favoriteCountry;
+  }
 }
