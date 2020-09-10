e5a04e6d0acf0bfec0a8c0d455c2d97bbbe25bd2
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-10 23:32:07 +0700

    add and update profile bloc

--- lib/bloc/bloc_profile.dart
+++ lib/bloc/bloc_profile.dart
@@ -1,12 +1,34 @@
 part of 'bloc.dart';
 
 class ProfileBloc implements Bloc {
+
+  final User user = locator.get<User>(instanceName: 'User Active');
+  final AuthService _authService = locator.get<AuthService>(instanceName: 'Auth Service');
+
+  final BehaviorSubject<File> _profilePictureController = BehaviorSubject();
+  Stream<File> get profilePictureStream => _profilePictureController.stream;
+
+  File _profileImage;
+  File get profileImage => _profileImage;
+
   @override
   void dispose() {
+    _profilePictureController.close();
   }
 
   @override
   Future<void> init() async {
+    
   }
 
+  Future<void> setProfilePicture() async {
+    await getImage().then((image) async {
+      user.photoURL = await uploadImage(image);
+      _profilePictureController.sink.add(image);
+    });
+  }
+
+  Future<void> signOut() async {
+    await _authService.signOut();
+  }
 }
