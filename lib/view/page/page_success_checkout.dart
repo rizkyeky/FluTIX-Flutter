d792487c5fb4566867031cfddac5c75169d68f16
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-10 23:33:51 +0700

    add popscope for disable back navigate

--- lib/view/page/page_success_checkout.dart
+++ lib/view/page/page_success_checkout.dart
@@ -5,34 +5,37 @@ class SuccessCheckoutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            height: 200,
-            child: const Icon(Icons.check_circle, color: mainColor, size: 54,),
-          ),
-          Text('Checkout Success', style: blueTitle),
-          Text("Enjoy your movie and\ndon't forget to bring your popcorns", 
-            style: blackSubtitle, textAlign: TextAlign.center
-          ),
-          Container(
-            alignment: Alignment.center,
-            height: 100,
-            child: FlatButton(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                  context, 
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                  (route) => false
-                );
-              }, 
-              child: Text('Back to home', style: blueSubtitle),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 200,
+              child: const Icon(Icons.check_circle, color: mainColor, size: 54,),
             ),
-          ),
-        ],
+            Text('Checkout Success', style: blueTitle),
+            Text("Enjoy your movie and\ndon't forget to bring your popcorns", 
+              style: blackSubtitle, textAlign: TextAlign.center
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 100,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                    context, 
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                    (route) => false
+                  );
+                }, 
+                child: Text('Back to home', style: blueSubtitle),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
