7bdfb26b21076060aad5b406b147108c90733fc6
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-11 22:46:57 +0700

    fix book time bloc

--- lib/bloc/bloc_booktime.dart
+++ lib/bloc/bloc_booktime.dart
@@ -2,16 +2,11 @@ part of 'bloc.dart';
 
 class BookTimeBloc implements Bloc {
 
-  // final MovieService _movieService = MovieService();
-
   final BehaviorSubject<int> _selectedDateController = BehaviorSubject();
   Stream<int> get selectedDateStream => _selectedDateController.stream;
 
   final BehaviorSubject<Map<String, int>> _selectedTimePlaceController = BehaviorSubject();
   Stream<Map<String, int>> get selectedTimePlaceStream => _selectedTimePlaceController.stream;
-
-  final BehaviorSubject<Map<String, int>> _selectedBookController = BehaviorSubject();
-  Stream<Map<String, int>> get selectedBookStream => _selectedBookController.stream;
 
   final DateTime _thisDate = DateTime.now();
   DateTime get thisDate => _thisDate;
@@ -20,7 +15,7 @@ class BookTimeBloc implements Bloc {
   List<Map<String, dynamic>> get daysInWeek => _daysInWeek;
   
   Map<String, dynamic> _selectedDate = {};
-  String get dayDate => '${_selectedDate['name'] as String} ${_selectedDate['date']} ${_thisDate.year}';
+  String get dayDate => '${_selectedDate['name']} ${_selectedDate['date']} ${_thisDate.year}';
   
   String _selectedPlace = '';
   String get place => _selectedPlace;
@@ -28,52 +23,33 @@ class BookTimeBloc implements Bloc {
   String _selectedTime = '';
   String get time => _selectedTime;
 
-
-  Map<String, String> get selectedDateTimePlace => {
-    'date': '${_selectedDate['name'] as String} ${_selectedDate['date']} ${_thisDate.year}',
-    'time': _selectedTime,
-    'place': _selectedPlace
-  };
-
-  Map<String, List<String>> timePlace = {
+  final Map<String, List<String>> _timePlace = {
     'XXI Paris Van Java Mall': ['10:00', '12:00', '14:00'],
     'XXI Bandung Indah Plaza': ['10:10', '12:10', '14:10'],
     'XXI Ciwalk': ['10:20', '12:20', '14:20'],
     'CGV Bandung Elektronik Center': ['10:30', '12:30', '14:30'],
   };
+  Map<String, List<String>> get timePlace => _timePlace;
 
-  bool isInit = false;
 
   @override
   void dispose() {
-    if (isInit) {
-      isInit = false;
-
-      _selectedDateController.close();
-      _selectedTimePlaceController.close();
-    }
+    _selectedTimePlaceController.close();
+    _selectedDateController.close();
   }
 
   @override
   Future<void> init() async {
-    if (!isInit) {
-      isInit = true;
-
-      getDaysInWeek();
+      _getDaysInWeek();
       _selectedDateController.sink.add(-1);
       _selectedTimePlaceController.sink.add({});
-    }
-  }
-
-  void printSelected() {
-    print('$_selectedDate $_selectedTime $_selectedPlace');
   }
 
   void selectDate(int index) {
 
     if (index == -1) {
+      _selectedDate = {};
       _selectedDateController.sink.add(index);
-
     }
     else if (_selectedDate != _daysInWeek[index]) {
       _selectedDate = _daysInWeek[index];
@@ -81,7 +57,7 @@ class BookTimeBloc implements Bloc {
     }
   }
 
-  void getDaysInWeek() {
+  void _getDaysInWeek() {
 
     int date = _thisDate.day;
     int weekday = _thisDate.weekday;
@@ -102,24 +78,24 @@ class BookTimeBloc implements Bloc {
       
       switch (weekday) {
         case 1: 
-          day['name'] = 'Mon'.toUpperCase();
+          day['name'] = 'Mon';
           break;
         case 2: 
-          day['name'] = 'Tus'.toUpperCase();
+          day['name'] = 'Tus';
           break;
         case 3: 
-          day['name'] = 'Wed'.toUpperCase();
+          day['name'] = 'Wed';
           break;
         case 4: 
-          day['name'] = 'Thus'.toUpperCase();
+          day['name'] = 'Thus';
           break;
         case 5: 
-          day['name'] = 'Fri'.toUpperCase();
+          day['name'] = 'Fri';
           break;
         case 6: 
-          day['name'] = 'Sat'.toUpperCase();
+          day['name'] = 'Sat';
           break;
-        default: day['name'] = 'Sun'.toUpperCase();
+        default: day['name'] = 'Sun';
       }
 
       day['date'] = date;
@@ -134,14 +110,23 @@ class BookTimeBloc implements Bloc {
   void selectTimePlace(String place, int indexTime) {
     
     if (indexTime == -1) {
+      _selectedPlace = '';
+      _selectedTime = '';
       _selectedTimePlaceController.sink.add({});
-      // print('$place $indexTime');
     }
     else {
-      _selectedTimePlaceController.sink.add({place: indexTime});
       _selectedPlace = place;
       _selectedTime = timePlace[place][indexTime];
-      // print('$place $indexTime');
+      _selectedTimePlaceController.sink.add({place: indexTime});
     }
+  }
+
+  void onSelectedBookTime() {
+    locator.call<Ticket>(instanceName: 'Ticket')
+      .copyWith(bookingDayDate: '${_selectedDate['name']} ${_selectedDate['date']} ${_thisDate.year}');
+    locator.call<Ticket>(instanceName: 'Ticket')
+      .copyWith(bookingPlace: _selectedPlace);
+    locator.call<Ticket>(instanceName: 'Ticket')
+      .copyWith(bookingTime: _selectedTime);
   }
 }
