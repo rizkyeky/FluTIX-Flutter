b8edabd2087edbc389261091af3aee1a97616b1d
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-13 21:31:46 +0700

    fix and finish edit profile page

--- lib/view/page/page_editprofile.dart
+++ lib/view/page/page_editprofile.dart
@@ -6,7 +6,13 @@ class EditProfilePage extends Page<EditProfileBloc> {
   void dispose() {}
 
   @override
-  void init() {}
+  void init() {
+    _nameText.text = bloc.user.name;
+    _emailText.text = bloc.user.email;
+  }
+
+  final TextEditingController _nameText = TextEditingController();
+  final TextEditingController _emailText = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,39 +22,100 @@ class EditProfilePage extends Page<EditProfileBloc> {
         textTitle: 'Profile',
         textStyle: blueTitle,
         backgroundColor: whiteColor,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: mainColor,), 
+          onPressed: () => Navigator.pop(context)
+        ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(24),
-              height: 240,
-              color: whiteColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: CircleAvatar(
-                      radius: 36,
-                      backgroundImage: (bloc.user.photoURL != '') 
-                      ? NetworkImage(bloc.user.photoURL)
-                      : const AssetImage('assets/no_image.png') as ImageProvider,
-                    )
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(bloc.user.name, style: blueSubtitle),
-                      Text(bloc.user.email, style: blackContentRegular),
-                    ],
-                  ),
-                ],
+      body: Builder(
+        builder: (contextScaffold) => SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(24),
+                height: 210,
+                width: MediaQuery.of(context).size.width,
+                color: whiteColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        bloc.setProfilePicture();
+                      },
+                      child: CircleAvatar(
+                        radius: 54,
+                        backgroundImage: (bloc.user.photoURL != '') 
+                        ? NetworkImage(bloc.user.photoURL)
+                        : const AssetImage('assets/no_image.png') as ImageProvider,
+                      )
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(bloc.user.name, style: blueSubtitle),
+                        Text(bloc.user.email, style: blackContentRegular),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        )
+              const SizedBox(height: 12,),
+              Container(
+                height: 240,
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                color: whiteColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    XTextField(
+                      controller: _nameText,
+                      text: 'Name',
+                    ),
+                    XTextField(
+                      controller: _emailText,
+                      text: 'Email',
+                    ),
+                    FlatButton.icon(
+                      label: Text('Save Changes', style: blueSubtitle,),
+                      icon: const Icon(Icons.check, color: mainColor),
+                      onPressed: () {
+                        if (_nameText.text != bloc.user.name || _emailText.text != bloc.user.email) {
+                          bloc.updateUser(_nameText.text, _emailText.text);
+                          Scaffold.of(contextScaffold).showSnackBar(snackBar(
+                            contentText: 'Saved',
+                            labelText: 'DISMISS',
+                            onPressed: () => Scaffold.of(contextScaffold).hideCurrentSnackBar()
+                          ));
+                        } else {
+                          Scaffold.of(contextScaffold).showSnackBar(snackBar(
+                            contentText: 'No Changed',
+                            labelText: 'DISMISS',
+                            onPressed: () => Scaffold.of(contextScaffold).hideCurrentSnackBar()
+                          ));
+                        }
+                      },
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 12,),
+              FlatButton(
+                onPressed: () async {
+                  await bloc.resetPassword().whenComplete(() {
+                    Scaffold.of(contextScaffold).showSnackBar(snackBar(
+                      contentText: 'The link to change your password has been sent to your email',
+                      labelText: 'DISMISS',
+                      onPressed: () => Scaffold.of(contextScaffold).hideCurrentSnackBar()
+                    ));
+                  });
+                },
+                child: Text('Reset Password', style: blackContentRegular,)
+              )
+            ],
+          )
+        ),
       ),
     );
   }
