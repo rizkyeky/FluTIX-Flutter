de189a3de4c35ca7644495eea4fc7b8bad0d178c
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-13 21:30:07 +0700

    add and update edit profile bloc

--- lib/bloc/bloc_editprofile.dart
+++ lib/bloc/bloc_editprofile.dart
@@ -3,11 +3,33 @@ part of 'bloc.dart';
 class EditProfileBloc implements Bloc {
 
   final User user = locator.get<User>(instanceName: 'User Active');
+  final AuthService _auth = locator.get<AuthService>(instanceName: 'Auth Service');
+  final UserService _userService = locator.get<UserService>(instanceName: 'User Service');
   
+  File _profilePicture;
+  File get profileImage => _profilePicture;
+
   @override
   void dispose() {}
 
   @override
   Future<void> init() async {}
 
+  Future<void> resetPassword() async {
+    _auth.resetPassword(user.email);
+  }
+
+  Future<void> updateUser(String name, String email) async {
+    user.copyWith(name: name, email: email);
+    await _userService.updateUser(user.id, name: name, email: email);
+  }
+
+  Future<void> setProfilePicture() async {
+    await getImage().then((value) async {
+      _profilePicture = value;
+      await uploadImage(_profilePicture).then((url) => 
+        user.copyWith(photoURL: url)
+      );
+    }).catchError((e) {});
+  }
 }
