786370004812fe5e506a9e29915309dfa53c1766
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-15 10:28:32 +0700

    add and update my ticket blo

--- lib/bloc/bloc_myticket.dart
+++ lib/bloc/bloc_myticket.dart
@@ -1,6 +1,12 @@
 part of 'bloc.dart';
 
 class MyTicketsBloc implements Bloc {
+  
+  final TicketService _ticketService = locator.get<TicketService>(instanceName: 'Ticket Service');
+  final User user = locator.get<User>(instanceName: 'User Active');
+  final DateTime _thisDate = DateTime.now();
+  
+
   @override
   void dispose() {
   }
@@ -9,4 +15,45 @@ class MyTicketsBloc implements Bloc {
   Future<void> init() async {
   }
 
+  Future<List<Ticket>> getNewestMovies() async {
+    final List<int> nowDayDate = [_thisDate.day,_thisDate.month,_thisDate.year];
+
+    List<Ticket> tickets = [];
+    tickets = await _ticketService.getTickets(user.id);
+    
+    if (tickets.isNotEmpty) {
+      tickets = tickets.where((element) {           
+        if (element.dayDate[2] < nowDayDate[2] 
+        || element.dayDate[1] < nowDayDate[1] 
+        || element.dayDate[0] < nowDayDate[0]) {
+          return true;
+        } else {
+          return false;
+        }
+      }).toList();
+    }
+
+    return tickets; 
+  }
+
+  Future<List<Ticket>> getOldestMovies() async {
+    final List<int> nowDayDate = [_thisDate.day,_thisDate.month,_thisDate.year];
+
+    List<Ticket> tickets = [];
+    tickets = await _ticketService.getTickets(user.id);
+    
+    if (tickets.isNotEmpty) {
+      tickets = tickets.where((element) {
+        if (element.dayDate[2] > nowDayDate[2] 
+        || element.dayDate[1] > nowDayDate[1] 
+        || element.dayDate[0] > nowDayDate[0]) {
+          return true;
+        } else {
+          return false;
+        }
+      }).toList();
+    }
+
+    return tickets; 
+  }
 }
