51e7fc888b8d7b9ccb3200ff236508834828b7ce
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-16 21:13:32 +0700

    fix sorting ticket

--- lib/bloc/bloc_myticket.dart
+++ lib/bloc/bloc_myticket.dart
@@ -6,7 +6,6 @@ class MyTicketsBloc implements Bloc {
   final User user = locator.get<User>(instanceName: 'User Active');
   final DateTime _thisDate = DateTime.now();
   
-
   @override
   void dispose() {
   }
@@ -16,19 +15,20 @@ class MyTicketsBloc implements Bloc {
   }
 
   Future<List<Ticket>> getNewestMovies() async {
-    final List<int> nowDayDate = [_thisDate.day,_thisDate.month,_thisDate.year];
+    final List<int> nowDayDate = [_thisDate.weekday,_thisDate.day,_thisDate.month,_thisDate.year];
 
     List<Ticket> tickets = [];
     tickets = await _ticketService.getTickets(user.id);
     
     if (tickets.isNotEmpty) {
-      tickets = tickets.where((element) {           
-        if (element.dayDate[2] < nowDayDate[2] 
+      tickets = tickets.where((element) {
+        if (element.dayDate[3] < nowDayDate[3] 
+        || element.dayDate[2] < nowDayDate[2] 
         || element.dayDate[1] < nowDayDate[1] 
         || element.dayDate[0] < nowDayDate[0]) {
-          return true;
-        } else {
           return false;
+        } else {
+          return true;
         }
       }).toList();
     }
@@ -37,16 +37,17 @@ class MyTicketsBloc implements Bloc {
   }
 
   Future<List<Ticket>> getOldestMovies() async {
-    final List<int> nowDayDate = [_thisDate.day,_thisDate.month,_thisDate.year];
+    final List<int> nowDayDate = [_thisDate.weekday,_thisDate.day,_thisDate.month,_thisDate.year];
 
     List<Ticket> tickets = [];
     tickets = await _ticketService.getTickets(user.id);
     
     if (tickets.isNotEmpty) {
       tickets = tickets.where((element) {
-        if (element.dayDate[2] > nowDayDate[2] 
-        || element.dayDate[1] > nowDayDate[1] 
-        || element.dayDate[0] > nowDayDate[0]) {
+        if (element.dayDate[3] < nowDayDate[3] 
+        || element.dayDate[2] < nowDayDate[2] 
+        || element.dayDate[1] < nowDayDate[1] 
+        || element.dayDate[0] < nowDayDate[0]) {
           return true;
         } else {
           return false;
