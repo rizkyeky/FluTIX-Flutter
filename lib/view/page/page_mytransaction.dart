9f8858a6ebc14a23124a726a9d3afefc6ff40e76
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-16 23:38:08 +0700

    add and update my transaction page

--- lib/view/page/page_mytransaction.dart
+++ lib/view/page/page_mytransaction.dart
@@ -21,21 +21,64 @@ class MyTransactionPage extends Page<MyTransactionBloc> {
         ),
       ),
       body: FutureBuilder<List<Transaction>>(
-        future: null,
-        builder: (context, snapshot) => (snapshot.hasData) 
-        ? (snapshot.data.isNotEmpty) ? ListView.builder(
-          itemCount: snapshot.data.length,
-          itemBuilder: (context, index) => ListTile(
-            // title: Text(snapshot.data[index].movie.title, style: blackSubtitle),
-            // subtitle: Text(snapshot.data[index].bookingPlace, style: blackContentRegular),
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              // child: Image.network('${imageBaseURL}w92${snapshot.data[index].movie.posterPath}')
-            ), 
+      future: bloc.getTransactions(),
+      builder: (context, snapshot) => (snapshot.hasData) 
+      ? (snapshot.data.isNotEmpty) ? ListView.separated(
+        itemCount: snapshot.data.length,
+        separatorBuilder: (context, index) => const Divider(
+          height: 1,
+          indent: 12,
+          endIndent: 12,
+        ),
+        itemBuilder: (context, index) => Material(
+          color: whiteColor,
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.network(
+                      '${imageBaseURL}w92${snapshot.data[index].picture}',
+                      fit: BoxFit.cover,
+                      height: 90,
+                      width: 60,
+                    )
+                  ),
+                  const SizedBox(width: 12),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 180,
+                        child: Text(snapshot.data[index].title, 
+                          style: blueTitle, 
+                          maxLines: 3,
+                          textAlign: TextAlign.left, 
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Text(
+                        snapshot.data[index].amount.toString(),
+                        style: blueSubtitle,
+                      ),
+                      Text(
+                        snapshot.data[index].subtitle, 
+                        style: blackContentRegular,
+                      ),
+                    ]
+                  )
+                ],
+              ),
+            ),
           ),
-        ) : const Center(child: Text('No Tickets')) 
-        : const Center(child: CircularProgressIndicator()) 
-      ),
+        )
+      ) : Center(child: Text('No Tickets', style: blackContentRegular,)) 
+      : const Center(child: CircularProgressIndicator()) 
+      )
     );
   }
 
