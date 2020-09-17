6d4308587f7417f31c3ad1671f66f3f132b2c7c0
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-17 09:43:01 +0700

    delete exetension

--- lib/extension/extension.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart' as fireAuth;
-import 'package:flutix_training/model/model.dart';
-import 'package:flutix_training/service/service.dart';
-
-part 'extension_firebase_user.dart';
-
--- lib/extension/extension_firebase_user.dart
+++
@@ -1,8 +0,0 @@
-part of 'extension.dart';
-
-// extension FirebaseUserExtension on fireAuth.User {
-//   User convertToUser(String name, List<String> favoriteGenre, List<String> favoriteCountry) => 
-//     User(uid, name, email, photoURL, favoriteGenre, favoriteCountry);
-
-//   Future<User> fromFireStore() async => await UserService.getUser(this.uid);
-// }
