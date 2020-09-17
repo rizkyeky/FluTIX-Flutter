572420ae6510cb0e7259706e3cd118511bdc35f1
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-17 10:55:01 +0700

    update page

--- lib/view/page/page.dart
+++ lib/view/page/page.dart
@@ -1,13 +1,12 @@
 library page;
 
-// import 'dart:io';
-
 import 'package:flutix_training/bloc/bloc.dart';
 import 'package:flutix_training/locator.dart';
-import 'package:flutix_training/view/component/component.dart';
 import 'package:flutix_training/model/model.dart';
-import 'package:flutter/material.dart';
 import 'package:flutix_training/share/share.dart';
+import 'package:flutix_training/view/component/component.dart';
+
+import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
